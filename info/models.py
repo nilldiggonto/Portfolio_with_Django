a6571fa3a9ac60b09fff388d110bc24878427bac
nilldiggonto <nilldiggonto@gmail.com>
2020-11-02 02:05:58 +0600

    Info Migrate

--- info/models.py
+++ info/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Skill(model.models):
-    
+class Skill(models.Model):
+    images =  models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+
